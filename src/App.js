289f362f2efc9bf68fab84e5ae50de7285cee427
Sarath Kumar <worksarath95@gmail.com>
2023-06-01 16:06:04 +0530

    basic setup done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Banner from './Components/Banner/Banner';
 import Rowpost from './Components/Rowpost/Rowpost';
+import {originals,action,comedy,romance,documentaries} from './urls';
 
 
 function App() {
@@ -10,7 +11,11 @@ function App() {
     <div className="App">
       <Navbar />
       <Banner />
-      <Rowpost />
+      <Rowpost url={originals} title='Netflix Orginals' />
+      <Rowpost url={action} title='Action' isSmall/>
+      <Rowpost url={romance} title='Romance' isSmall/>
+      <Rowpost url={comedy} title='Comedy' isSmall />
+      <Rowpost url={documentaries} title='Documentries' isSmall />
     </div>
   );
 }
--- src/Components/Rowpost/Rowpost.js
+++ src/Components/Rowpost/Rowpost.js
@@ -1,30 +1,53 @@
 import React, { useEffect, useState } from 'react'
 import './Rowpost.css'
 import axios from '../../axios'
-import { API_KEY,imageUrl } from '../../constants/constants'
+import {API_KEY, imageUrl } from '../../constants/constants'
+import Youtube from 'react-youtube';
 
 
-function Rowpost() {
+function Rowpost(props) {
   const [movieRow, setRow] = useState([])
-
+  const [urlId, setUrlId] = useState('')
   useEffect(() => {
-    axios.get(`discover/tv?api_key=${API_KEY}&with_networks=213`).then((response) => {
+    axios.get(props.url).then((response) => {
       //console.log("row movie list is ",response.data.results)
       setRow(response.data.results) 
     }).catch(err => {
       alert("Network Error")
     })
-  },[])
+  },[props.url])
+
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 0,
+    },
+  };
+
+  const handleMovieTrailer = (id) => {
+    //console.log(id)
+    axios.get(`movie/${id}/videos?api_key=${API_KEY}`).then(response => {
+      if (response.data.results.length !== 0){
+        setUrlId(response.data.results[0])
+      }
+      else{
+        alert("No Trailer Found!")
+      }
+    })
+  }
 
   return (
     <div className='row'>
-        <h2>Netflix Originals</h2>
-        <div className='posters'>
+        <h2>{props.title}</h2>
+        <div className='posters' key={props.id}>
           {movieRow.map((movie) => 
-            <img className='poster' src={`${imageUrl + movie.backdrop_path}`} alt="poster" />
+            <img key={movie.id} onClick={() => handleMovieTrailer(movie.id)}  className={props.isSmall ? 'smallPoster': 'poster'} src={`${imageUrl + movie.backdrop_path}`} alt="poster" />
           )}
             
         </div>
+       {urlId && <Youtube opts={opts} videoId={urlId.key} /> } 
     </div>
   )
 }
